39cd90bcdd50fd569108333300cf0c53223e8d33
prasad <pfirame18@gmail.com>
2024-05-17 00:29:59 +0530

    made navbar componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import './index.css';
 const App = () => {
   return (
     <div className='app'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import './Navbar.css';
 const Navbar = () => {
+    const [menu,setmenu]=useState("home");
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <img src={assets.logo} alt='' className='logo'/>
+        <ul className='navbar-menu'>
+        <li className={menu==="home"?"active":""}>home</li>
+        <li className={menu==="menu"?"active":""}>menu</li>
+        <li className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li className={menu==="contact-us"?"active":""}>contact us</li>
+        </ul>
+         <div className='navbar-right'>
+            <img src={assets.search_icon} alt="" />
+            <div className='navbar-search-icon' alt="" />
+                 <img src={assets.basket_icon} />
+                 <div className='dot'></div>
+            </div>
+            <button>Sign in</button>
+         </div>
+
   )
 }
 
-export default Navbar
+export default Navbar;
