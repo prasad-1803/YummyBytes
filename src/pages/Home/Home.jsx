6d0629a34b365e44a6ddabf6db3ad2a5e1e432d2
prasad <pfirame18@gmail.com>
2024-05-17 10:19:45 +0530

    added header component inside home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import "./Home.css"
+import Header from '../../components/Header.jsx/Header';
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    <Header />
+    </div>
   )
 }
 
