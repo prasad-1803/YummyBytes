debbcebb7dbfda0d7ebb76f9efd43bc238db9571
prasad <pfirame18@gmail.com>
2024-05-17 07:47:28 +0530

    onclick fun added to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} alt='' className='logo'/>
         <ul className='navbar-menu'>
-        <li className={menu==="home"?"active":""}>home</li>
-        <li className={menu==="menu"?"active":""}>menu</li>
-        <li className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li className={menu==="contact-us"?"active":""}>contact us</li>
+        <li onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setmenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setmenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setmenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
         </ul>
          <div className='navbar-right'>
             <img src={assets.search_icon} alt="" />
